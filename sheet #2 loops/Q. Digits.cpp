c97de655befa6563fae2312a4eae66ee242a0c3a
Mohamed-7018 <80456446+Mohamed-7018@users.noreply.github.com>
2022-09-29 05:46:13 +0200

    update digits problem

--- sheet #2 loops/Q. Digits.cpp
+++ sheet #2 loops/Q. Digits.cpp
@@ -1,46 +1,57 @@
+#include <bits/stdc++.h>
 
-#include <iostream>
-#include <cmath>
+using namespace std;
+#define ll  long long
+#define ld  long double
+#define mk  make_pair
+#define vi  vector<int>
+#define vs  vector<string>
+#define vp  vector<pair<string, string>>
+#define all(v) v.begin(),v.end()
+#define vl  vector<long long>
+#define pqi priority_queue<int>
+#define qi  queue<int>
+#define sc  set<char>
+#define si  set<int>
+#define pii pair<int, int>
+#define psv pair<string, vector<int>>
+#define mci map<char,int>
+#define msv map<string,vector<string>>
+#define sz(s)    (int)(s.size())
+
+
 using namespace std;
 
 
-int main () {
-    int n,k;
-    cin>> n;
-    
-    
-    while(n>0) {
-        int x, count =0, reverse=0, rem;
-        cin>> x;
-        
-        int a =x;
-        while (a>0) {
-            a= a/10;
-            count++;
-        }
-        // cout << count<< " ";
-        
-      while(x!=0)    {    
-           rem=x%10;      
-           reverse=reverse*10+rem;    
-           x/=10;    
-        }    
-      
+void begin() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-
-      for(int j=count-1; j>=0; --j){
-			
-			if(j == 0){
-				cout<<reverse;
-				break;
-			}
-			else{
-				int k = pow(10,j);
-				cout<<reverse/k<<" ";
-				reverse = reverse%k;
-			}
-		} 
-       cout <<endl;
-       --n;
-    }
 }
+
+int main() {
+
+    begin();
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        string num;
+        cin >> num;
+        for (int i= num.size() -1;i >=0; i--){
+            cout << num[i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+
+}
+
+
+
+
+
+// g++ -o hello hello.cpp
+// ./hello
